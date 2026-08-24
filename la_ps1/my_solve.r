b2903a934f43a5c42f5d6ddc68474966ebdccae3
jmwingenroth <jwing421@gmail.com>
2026-08-23 23:21:30 -0700

    Add `my_solve()`

---
+++ la_ps1/my_solve.r
@@ -0,0 +1,24 @@
+# Return solution to system of equations if matrix is nonsingular
+my_solve <- function(A, b) {
+
+    # Catch output
+    tryCatch({
+
+        # Solve and return column vector
+        t(t(solve(A, b)))
+
+    }, error = function(e) {
+
+        # Get error message
+        msg <- conditionMessage(e)
+
+        # Reformat to match spec from problem set
+        if (grepl("exactly singular", msg)) {
+
+            print("It does not have a unique solution")
+
+        }
+
+    })
+
+}
